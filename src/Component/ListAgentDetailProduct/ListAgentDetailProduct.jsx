b733c7989f96dc73c5801b5e3dc4bb08ded30f2a
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-05-28 00:07:13 +0700

    Update

--- src/Component/ListAgentDetailProduct/ListAgentDetailProduct.jsx
+++ src/Component/ListAgentDetailProduct/ListAgentDetailProduct.jsx
@@ -2,35 +2,44 @@ import React from "react";
 import { Label } from 'semantic-ui-react';
 import './ListAgentDetailProduct.css';
 
-function ListAgentDetailProduct(){
+function ListAgentDetailProduct(props){
+
+    let [content , setContent] = React.useState();
+    let [agent , setAgent ] = React.useState([]);
+
+    React.useEffect(()=>{
+        if(agent.length !== props.data.length){
+            props.data.map((data , index)=>{
+                            console.log(data)
+                fetch(`https://valorant-api.com/v1/agents/${data}`)
+                .then((response) => response.json())
+                .then((res)=>{
+                    let addArray = [];
+                    addArray = agent;
+                    addArray.push(res.data);
+                    setAgent(addArray);
+                }).then(()=>{
+                    setContent(
+                        agent.map((data , index)=>{
+                            console.log(data)
+                            return(<Label as='a' key={index}>
+                                <img src={data.displayIconSmall} />
+                                {data.displayName}
+                            </Label> )
+                        })
+                    )
+                });
+            });
+        }
+    },[]);
+
     return(
         <div className="list-skin-detail-product">
             <h4>Agent</h4>
             <div className="container-list-skin-detail-product agent">
-                <Label as='a'>
-                    <img src='https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/displayiconsmall.png' />
-                    Brimstone``
-                </Label> 
-                <Label as='a'>
-                    <img src='https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/displayiconsmall.png' />
-                    Fade
-                </Label> 
-                <Label as='a'>
-                    <img src='https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/displayiconsmall.png' />
-                    Fade
-                </Label> 
-                <Label as='a'>
-                    <img src='https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/displayiconsmall.png' />
-                    Fade
-                </Label> 
-                <Label as='a'>
-                    <img src='https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/displayiconsmall.png' />
-                    Fade
-                </Label> 
-                <Label as='a'>
-                    <img src='https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/displayiconsmall.png' />
-                    Fade
-                </Label> 
+                {content}
+                
+             
             </div>
         </div>
     );
