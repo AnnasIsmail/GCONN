45b9fe2cdf81bd5c585e9049d031a48943624760
Annas Komputer <annasismailmuhammad@gamil.com>
2023-09-09 10:29:54 +0700

    Fix FormatMoney.js

--- src/Function/FormatMoney.js
+++ src/Function/FormatMoney.js
@@ -1,11 +1,15 @@
+// Format uang rupiah dari integer
+const FormatMoney = ({money}) => {
+  const integerAmount = parseInt(money, 10);
+  if (isNaN(integerAmount)) {
+    throw new Error('Input must be Number!');
+  }
+  const formattedNumber = new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0,
+  }).format(integerAmount);
 
-function FormatMoney(props){
-
-    return new Intl.NumberFormat("id-ID", {
-        style: "currency",
-        currency: "IDR"
-      }).format(props.money);
-
-}
-
+  return formattedNumber;
+};
 export default FormatMoney;
